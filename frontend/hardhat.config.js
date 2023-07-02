21227a403163e5f1fce7447fc1888f109c8a03db
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2023-07-02 22:30:46 +0400

    Set Sepolia network node for deploy contract

--- frontend/hardhat.config.js
+++ frontend/hardhat.config.js
@@ -6,14 +6,19 @@ module.exports = {
   solidity: "0.8.18",
   defaultNetwork:"hardhat",
   networks:{
-    hardhat:{
-      accounts:[
-        {
-          privateKey:process.env.HARDHAT_ACCOUNT_PRIVATE_KEY,
-          balance:"1000000000000000000000",
-        },
-      ],
-      chainId:31337
+    // hardhat:{
+    //   accounts:[
+    //     {
+    //       privateKey:process.env.HARDHAT_ACCOUNT_PRIVATE_KEY,
+    //       balance:"1000000000000000000000",
+    //     },
+    //   ],
+    //   chainId:31337
+    // }
+    sepolia:{
+      url:process.env.SEPOLIA_NODE_URL_HTTPS,
+      accounts:[process.env.METAMASK_PRIVATE_KEY],
+      chainId:11155111
     }
   }
 };
